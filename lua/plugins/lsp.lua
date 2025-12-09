99c0c497e5ac871d87b4e3bf46db42b5610195bb
monsterooo <createthink.net@gmail.com>
2025-12-09 16:19:49 +0800

    init lazygit

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,14 @@
+return {
+  -- Auto-install LSP servers
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "typescript-language-server",
+  --       "css-lsp",
+  --       "tailwindcss-language-server",
+  --       "biome", -- Fast formatter/linter
+  --     })
+  --   end,
+  -- },
+}
